8ca40c6905f0522a79d65741782755a29d22b1ea
Karsten Johansson <KarstenJohansson@gmail.com>
2018-11-14 10:51:21 -0500

    Update cardlib.lisp

--- cardlib.lisp
+++ cardlib.lisp
@@ -1,6 +1,19 @@
-;;;; Utility bits that might be useful outside of Committed
+;;;; Domain Specific Language for working with playing cards
 
-(defconstant suit-order
-  '#("C" "H" "S" "D") "Default suit order")
+(defconstant +chased-suits+
+  '#("Clubs" "Hearts" "Spades" "Diamonds") "CHaSeD suit order")
+
+(defconstant +pips+
+  '#("♣" "♥" "♠" "♦") "Unicode pips")
    
-   
+(defconstant +red-cards+
+  '#("Hearts" "Diamonds") "Red cards")
+
+(defconstant +black-cards+
+  '#("Clubs" "Spades") "Red cards")
+
+(defconstant +card-ranks+
+  '#("Ace" 2 3 4 5 6 7 8 9 10 "Jack" "Queen" "King") "Card Ranks in order, without jokers")
+
+
+
